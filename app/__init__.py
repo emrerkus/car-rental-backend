9499e48c0c7bd16df5be96794b76d1ec0c07d5b3
emrerkus <emrerkus01@gmail.com>
2025-07-25 15:05:14 +0300

    program initialization added

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask
 from app.db import db
 from app.routes import auth_routes, car_routes, user_routes, rental_routes, admin_routes
 
 
-def create_app():
+def create_app(username, password, database_port, database_name):
     app = Flask(__name__)
 
     # Connects database to the flask app
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3406@localhost:5432/testdb'
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{password}@localhost:{database_port}/{database_name}'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3406@localhost:5432/testdb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
--- run.py
+++ run.py
@@ -2,23 +2,39 @@ from app import create_app
 from app.db import db
 from app.models import User
 from werkzeug.security import generate_password_hash
+import sys
 
-app = create_app()
 
-with app.app_context():
-    db.create_all()
-    admin_user = User.query.filter_by(username='admin').first()
-    if not admin_user:
-        new_admin = User(
-            username='admin',
-            password=generate_password_hash('admin123'),
-            role='admin'
-        )
-        db.session.add(new_admin)
-        db.session.commit()
-        print("Admin has been created")
-    else:
-        print("ERROR")
+username = None
+password = None
+database_port = None
+database_name = None
+
 
 if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("Usage: python run.py <username> <password> <database_port> <database_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_port = sys.argv[3]
+    database_name = sys.argv[4]
+    app = create_app(username, password, database_port, database_name)
+
+    with app.app_context():
+        db.create_all()
+        admin_user = User.query.filter_by(username='admin').first()
+        if not admin_user:
+            new_admin = User(
+                username='admin',
+                password=generate_password_hash('admin123'),
+                role='admin'
+            )
+            db.session.add(new_admin)
+            db.session.commit()
+            print("Admin has been created")
+        else:
+            print("ERROR")
+
     app.run(debug=True)
